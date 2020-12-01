0f3575ba606f7636a85dc939dcf65efa15634fe0
pat-white-code <pat.k.white@gmail.com>
2020-11-30 21:27:28 -0600

    adds store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 import ImageGrid from './components/ImageGrid'
+// import {Provider } from 'react-redux';
+
+import configureStore from './redux/store';
+
+// const store = configureStore();
+
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <ImageGrid />
-    </div>
+      <div>
+        <Header/>
+        <ImageGrid />
+      </div>
   );
 }
 
